8f0a4f49076879db88e544c32c3a59a1c01fc2d5
Ignacio Paradisi <ignacioparadisi@gmail.com>
2024-04-24 13:39:52 -0600

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Ignacio Paradisi on 4/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
